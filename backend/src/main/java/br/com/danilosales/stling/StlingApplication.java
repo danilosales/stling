8e79188d226baa3baef2343ebf3ffd4c40a55e55
Danilo Sales <danilosales.info@gmail.com>
2019-09-04 14:11:37 -0300

    Initial structure

---
+++ backend/src/main/java/br/com/danilosales/stling/StlingApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilosales.stling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlingApplication.class, args);
+	}
+
+}
